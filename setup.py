c4714574e87e2725f740a7133b35f409b7ae2ecd
WenDawson <dawsonwen123@gmail.com>
2024-10-29 22:41:24 +0800

    add yolo 2 coco

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version='0.0.1',
     install_requires=[
         'numpy',
+        'imagesize',
         'importlib-metadata; python_version<"3.10"',
     ],
-    packages=find_packages(
-        where="cocoviewer"
-    ),
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'cocoviewer = cocoviewer.cocoviewer:main',
+            'cocofromyolo = cocoviewer.coco_cvt_from_yolo:yolo2coco',
         ]
     }
 )
